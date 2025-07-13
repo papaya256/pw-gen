c3199607b241b8b4a95c3801c3dd6937e650eae0
thefueley <5083152+thefueley@users.noreply.github.com>
2025-07-13 15:00:33 -0400

    added all args

--- pw-gen.py
+++ pw-gen.py
@@ -1,19 +1,45 @@
+#!/usr/bin/env python3
 import argparse
 import string
 import random
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--length", help="desired password length (integer), type=int")
+    parser = argparse.ArgumentParser(
+        description="Generate a random password with specified length and character types.",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
 
-    parser.add_argument("--verbose", help="increase output verbosity",
-                        action="store_true")
-    
+    parser.add_argument(
+        "-l", "--length",
+        type=int,
+        default=12,
+        help="Desired password length (integer). Default is 12."
+    )
+
+    parser.add_argument(
+        "-u", "--upper",
+        action="store_true",
+        help="Include uppercase letters (A-Z)."
+    )
+    parser.add_argument(
+        "-o", "--lower",
+        action="store_true",
+        help="Include lowercase letters (a-z)."
+    )
+
+    parser.add_argument(
+        "-n", "--numbers",
+        action="store_true",
+        help="Include numbers (0-9)."
+    )
+
+    parser.add_argument(
+        "-s", "--special",
+        action="store_true",
+        help="Include special characters (e.g., !@#$*()-+=[]{}|;:,.<>?/)."
+    )
+
     args = parser.parse_args()
-    if args.length:
-        print("length: {}".format(args.length))
-    if args.verbose:
-        print("verbosity turned on")
 
 
 if __name__ == "__main__":
